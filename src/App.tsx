54841be8a010cc0fe22e010d4ca211710e8d1bf2
NJesus144 <nalbert-edm@outlook.com>
2024-03-30 10:20:16 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Container, CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <h1>Dark Mode</h1>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
